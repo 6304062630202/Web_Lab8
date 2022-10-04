fe7f19be0c6b479b04ed45163411877b65525549
6304062630202 <94558432+6304062630202@users.noreply.github.com>
2022-10-04 18:48:01 +0700

    Update Lab8_5.php

--- Lab8_5.php
+++ Lab8_5.php
@@ -2,18 +2,18 @@
 <html>
 <head><meta charset="utf-8"></head>
 <body>
+<div style="display:flex">
 <?php
     $stmt = $pdo->prepare("SELECT * FROM member");
     $stmt->execute();
 ?>
 <?php while ($row = $stmt->fetch()) : ?>
-    ชื่อสมาชิก :
+    <div style="padding: 15px; text-align: center">
     <a href="detail.php?num=<?=$row["num"]?>">
-        <?=$row ["name"]?>
+        <img src='img/<?=$row["num"]?>.jpg' width='100'>
     </a><br>
-    ที่อยู่ : <?=$row ["address"]?><br>
-    อีเมล์ : <?=$row ["email"]?><br>
-    <img src='img/<?=$row["num"]?>.jpg' width='100'><br><hr>
+    <?=$row ["username"]?>
+    </div>
 <?php endwhile; ?>
 </body>
 </html>
